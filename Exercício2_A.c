9271dc5f24ed76fa9b8109574b714a037aff335f
matheusmontemor <math.montemor@gmail.com>
2024-09-09 23:19:03 -0300

    Exercício2_A.c

---
+++ Exercício2_A.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    int a, b, c;
+    scanf( "%i" , &a);
+    scanf( "%d" , &b);
+    scanf( "%d" , &c);
+    printf( "%d\n" , c);
+    printf( "%d\n" , b);
+    printf( "%d\n" , a);
+
+    return 0;
+}
