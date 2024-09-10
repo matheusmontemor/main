aad5800e3806826f469052c138e748b5c4ce4f66
matheusmontemor <math.montemor@gmail.com>
2024-09-09 23:19:28 -0300

    Exercício2_B.c

---
+++ Exercício2_B.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    char nome[30];
+    printf( "Ola eu sou o No 1, como eh seu nome? " );
+    scanf( "%s", nome);
+    printf( "Bem-vindo ao clube %s ", nome);
+    return 0;
+
+}
