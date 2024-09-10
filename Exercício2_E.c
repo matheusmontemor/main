8b64cb88a5caae0dfeceb584722784d41e534304
matheusmontemor <math.montemor@gmail.com>
2024-09-09 23:20:40 -0300

    Exercício2_E.c

---
+++ Exercício2_E.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+    float latas ;
+    float altura, largura;
+
+    printf("digit a altura: ");
+    scanf ( "%f" , &altura );
+    printf("digit a largura: ");
+    scanf ( "%f" , &largura );
+    printf("%f",largura * altura * 0.15 );
+
+
+    return 0;
+}
